309e6491978ad90fffdcd759725457f2c2fc149c
Sam Mason <samm@lostmy.name>
2017-02-15 14:23:02 +0000

    Remove V1 functionality and docs

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express')
-const httpProxy = require('http-proxy')
 const nunjucks = require('nunjucks')
 
 const app = express()
-const apiV1Proxy = httpProxy.createProxyServer()
 const isDev = process.env.NODE_ENV !== 'production'
 
 app.use(express.static('public'))
@@ -17,10 +15,6 @@ nunjucks.configure('views', {
 app.get('/', (req, res) => res.render('index.html'))
 app.get('/documentation/v1', (req, res) => res.render('docsV1.html'))
 app.get('/documentation/v2', (req, res) => res.render('docsV2.html'))
-
-app.get('/api/v1/*', (req, res) => {
-  apiV1Proxy.web(req, res, { target: 'https://178.62.59.174', secure: false })
-})
 
 const getPort = process.env.PORT || 3333
 
